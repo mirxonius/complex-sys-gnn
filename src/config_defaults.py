c4a03e106c9275966ee54419fdca0a90650d3bf7
mirksonius <mirksonius@pop-os.localdomain>
2023-10-18 21:42:29 +0200

    dataset, config_defaults and renaming

--- src/config_defaults.py
+++ src/config_defaults.py
@@ -6,3 +6,15 @@ class SupportedModels(Enum):
     gate_equiv_model = "gate_equiv_model"
     mace_model = "mace_model"
     equivariant_gat = "equivariant_gat"
+
+
+class SupportedDatasets(Enum):
+    QM9 = "QM9"
+    top_tagging = "top_tagging"
+    flag = "flag"
+    aero = "aero"
+
+
+batch_size = 128
+num_epochs = 20
+lr = 1e-2
--- src/datasets/QM9_dataset.py
+++ src/datasets/QM9_dataset.py
@@ -1,18 +1,32 @@
-from typing import Optional, Callable
+from typing import Optional, Callable, Iterable
 from pathlib import Path
-from torch.utils.data import Dataset
+import torch
+from torch.utils.data import Dataset, Subset
 from torch_geometric.datasets import QM9
 
 
 class QM9_dataset(Dataset):
     def __init__(
-        self, root_dir: str | Path, transforms: Optional[Callable] = None
+        self,
+        root_dir: str | Path,
+        transforms: Optional[Callable] = lambda x: x,
+        indices: str | Path | Iterable = None,
     ) -> None:
         super().__init__()
-        self.ds = QM9(root_dir)
+        self.data = QM9(root_dir)
+        self.transforms = transforms
+        if isinstance(indices, Path) or isinstance(indices, str):
+            indices = torch.load(indices)
+        assert isinstance(indices, Iterable), f"{type(indices)} must be an iterable."
+        self.indices = (
+            torch.arange(len(self.data), dtype=int) if indices is None else indices
+        )
+        self.data = Subset(self.data, self.indices)
 
     def __len__(self):
-        return len(self.ds)
+        return len(self.data)
 
-    def __getitem__(self, index) -> Any:
-        return NotImplemented
+    def __getitem__(self, index):
+        graph = self.data[index]
+        graph = self.transforms(graph)
+        return graph
