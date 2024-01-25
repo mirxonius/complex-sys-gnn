b7cf171c50a76f1fb21e01cfe4322ee085f88700
Filip Mirkovic <filip.mirkovic@atmc.ai>
2024-01-25 13:23:45 +0100

    basic classes for md17 datsets

---
+++ src/datasets/Md17_dataset.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import torch
+from torch.utils.data import Dataset
+from torch_geometric.datasets import MD17
+from torch_geometric.data import Data
+
+
+class BenzeneEntanolUracilDataset(Dataset):
+    pass
+
+
+class AspirinDataset(Dataset):
+    pass
