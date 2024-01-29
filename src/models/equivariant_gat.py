87669c8928334ffca89057e1e71797077108c83e
mirksonius <mirksonius@pop-os.localdomain>
2024-01-29 12:43:01 +0100

    fixed problems with encoder and decoder

--- src/models/equivariant_gat.py
+++ src/models/equivariant_gat.py
@@ -24,24 +24,31 @@ class O3GraphAttentionNetwork(torch.nn.Module):
         super().__init__()
         self.aggregate = MeanOnGraph() if aggregate else IdentityOnGraph()
         self.embedding_layer = torch.nn.Linear(num_atom_types, embedding_size)
+        self.node_encoder = NodeEncoder(
+            embedding_irreps=hidden_irreps,
+            num_basis=num_basis,
+            max_radius=self.max_radius,
+        )
         layers = []
         for i in range(num_layers):
             layers.append(
                 O3AttentionLayer(
-                    input_irreps=hidden_irreps if i > 0 else input_irreps,
+                    input_irreps=hidden_irreps,
                     key_irreps=hidden_irreps,
                     query_irreps=hidden_irreps,
-                    value_irreps=hidden_irreps if i < num_layers - 1 else output_irreps,
+                    value_irreps=hidden_irreps,
                     lmax=lmax,
                     num_basis=num_basis,
                     max_radius=self.max_radius,
                 )
             )
         self.layers = torch.nn.ModuleList(layers)
+        self.decoder = o3.Linear(irreps_in=hidden_irreps, irreps_out=output_irreps)
 
     def forward(self, graph: Data) -> torch.Tensor:
-        graph.x = self.embedding_layer(graph.z)
+        graph.x = self.node_encoder(graph)
         for layer in self.layers:
             updated_node_features = layer(graph)
-            graph.x = updated_node_features
-        return self.aggregate(updated_node_features, batch_index=graph.batch)
+            graph.x += updated_node_features
+        graph.x = self.decoder(graph.x)
+        return self.aggregate(graph.x, batch_index=graph.batch)
